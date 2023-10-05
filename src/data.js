3136beb203474ffad23e14b14847610b181664a5
eliana osorio <yasminosorio2013@gmail.com>
2023-10-04 21:14:20 -0500

    fix: remove bearer token

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+import {bearerToken} from './secrets.js';
 export const search = async (searchmovie) => {
 
   const endPoint = `https://api.themoviedb.org/3/search/collection?query=${searchmovie}&include_adult=false&language=en-US&page=1`;
@@ -7,7 +8,7 @@ export const search = async (searchmovie) => {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OTk0MGRlNGMwNjBhMjViNWNmNTc0OWYyN2UxNmQ0NSIsInN1YiI6IjY1MDhjMDQzODI2MWVlMDBhY2ZlMTQzYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oislEXDAJU7J-bPwc1cSbx97xWVIsvsNTP0HNhnkfEM'
+      Authorization: bearerToken
     }
   };
     
