63447e2f6bc1edf2b51637a39a300039eb5e7c4f
eliana osorio <yasminosorio2013@gmail.com>
2023-10-12 15:48:44 -0500

    feat: create function list favourites and printFavourite

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
-import {my_Api_key} from './secrets.js';
+import {myApiKey} from './secrets.js';
 export const search = async () => {
 
-  const endPoint = `https://api.thecatapi.com/v1/images/search?limit=10&api_key=${my_Api_key}`;
+  const endPoint = `https://api.thecatapi.com/v1/images/search?limit=10&api_key=${myApiKey}`;
     
     
   const options = {
@@ -12,4 +12,43 @@ export const search = async () => {
   return fetch(endPoint, options)
     .then(response => response.json())
     .catch(err => console.error(err));
+}
+
+export const saveFavourite = async (catId) => {
+
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("x-api-key", myApiKey);
+
+  const raw = JSON.stringify({
+    "image_id": catId
+  });
+
+  const requestOptions = {
+    
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.thecatapi.com/v1/favourites", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+
+export const listFavourite = async () => {
+
+  const endPointFav = `https://api.thecatapi.com/v1/favourites`;
+
+  const options = {
+    method: 'GET',
+    headers:{'x-api-key': myApiKey},
+    redirect: 'follow'
+  };
+  
+  return fetch(endPointFav, options)
+    .then(response => response.json())
+    .catch(error => console.log('error', error))
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {search} from './data.js';
+import {listFavourite, saveFavourite, search} from './data.js';
 
 
 const printData = (cats) => {
@@ -16,8 +16,6 @@ const printData = (cats) => {
   // con este llamo el id del div donde quiero crear el div nuevo y le digo con el appendChild que sea hijo de este mismo.
   document.getElementById('dataPrint').appendChild(divNew);
 
-
-
   //creo el forEach que va a iterar y crear los elementos a mostrar de la API. 
   cats.forEach(element => {
     const template = `
@@ -27,11 +25,21 @@ const printData = (cats) => {
         <img src="${element.url}" width="500">
         </div>
         <div class="favorite">
-        <button class="btnFav" >Agregar a favoritos</button>
+        <button class="btnFav" id="${element.id}">Agregar a favoritos</button>
         </div>
     </div>`;
 
     divNew.innerHTML += template
+
+  });
+
+  divNew.querySelectorAll('.btnFav').forEach(btn =>{
+    btn.addEventListener("click", async () => {
+      const catId = btn.id;
+      await saveFavourite(catId)
+        .then(result => console.log(result))
+        .catch(error => console.error(error));
+    });
   });
 } 
 
@@ -39,5 +47,45 @@ const callButtonMovie = document.getElementById('searchBtn');
 callButtonMovie.addEventListener("click", async () => {
   await search()
     .then(result => printData(result))
+    .catch(error => console.error(error));
+});
+
+const printFavouriteCat = (cats) => {
+
+  console.log(cats);
+  //con este if valido si el div ya existe y si existe que lo elimine para volverlo a crear
+  if (document.getElementById('container')){
+
+    const container = document.getElementById('container');
+    document.getElementById('dataPrint').removeChild(container);
+  }
+  //con este createElement creo el elemento div nuevo y con el setAttribute le doy el atributo id al nuevo div.
+  const divNew = document.createElement('div');
+  divNew.setAttribute('id','container');
+
+  // con este llamo el id del div donde quiero crear el div nuevo y le digo con el appendChild que sea hijo de este mismo.
+  document.getElementById('dataPrint').appendChild(divNew);
+
+  //creo el forEach que va a iterar y crear los elementos a mostrar de la API. 
+  cats.forEach(element => {
+    const template = `
+    <div class="movieCard">
+        <div class="titleMovie">${element.image_id}</div>
+        <div class="images" >
+        <img src="${element.image.url}" width="500">
+        </div>
+        <div class="favorite">
+        <button class="deleteFav" id="${element.id}">Eliminar de favoritos</button>
+        </div>
+    </div>`;
+
+    divNew.innerHTML += template
+  });
+} 
+
+const callButtonFav = document.getElementById('favouriteBtn');
+callButtonFav.addEventListener("click", async () => {
+  await listFavourite()
+    .then(result => printFavouriteCat(result))
     .catch(error => console.error(error));
 });
